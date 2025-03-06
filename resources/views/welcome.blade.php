f7403201f94dc18b3390318f8e50235d66d35c6a
Anna Clara Moreira <clarapbi181@gmail.com>
2025-03-06 16:01:25 -0300

    Alterando titulo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Supermercado</title>
+        <title>Supermercado Dona Balbina</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
